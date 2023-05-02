9b28e7e3e291b0f0c32f4bcfd032e193d57c9bd8
Teddy <teddywambua5456@gmail.com>
2023-05-02 07:59:12 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Movies from './Movies';
+import Directors from './Directors';
+import Actors from './Actors';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,21 @@
-import React from "react";
-import { directors } from "../data";
+import { directors } from '../data';
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => (
+        <div key={director.id}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map((movie) => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
